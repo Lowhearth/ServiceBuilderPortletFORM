920e9922f2fe67360f418ed680100a354b3d6361
Lowhearth <hearthisinyourmind@gmail.com>
2018-08-31 16:27:42 +0200

    tjks

--- FihocaServiceBuilder-portlet/src/main/java/com/form/Formulary.java
+++ FihocaServiceBuilder-portlet/src/main/java/com/form/Formulary.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
-import javax.portlet.GenericPortlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequestDispatcher;
-import javax.portlet.ProcessAction;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
@@ -19,8 +17,7 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
 import com.example.plugins.model.Employee;
 import com.example.plugins.model.impl.EmployeeImpl;
 import com.example.plugins.service.EmployeeLocalServiceUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
+
 
 /**
  * Portlet implementation class Formulary
@@ -38,17 +35,21 @@ public class Formulary extends MVCPortlet {
         include(viewTemplate, renderRequest, renderResponse);
     }
     
-    @ProcessAction(name="AddEmployee") 
-  //This method is called when a <portlet:actionURL name="MyActionMethod"/> if processAction is not override
-    public void AddEmployee(ActionRequest request, ActionResponse response ) throws IOException, PortletException, SystemException {
-    	
+
+    public void AddEmployee(ActionRequest request, ActionResponse response ) throws IOException, PortletException {
+    	try {
     		Employee employee = new EmployeeImpl();
     		employee.setName((String) request.getParameter("name"));
     		employee.setAddress((String) request.getParameter("address"));
-    		employee.setEid((int)CounterLocalServiceUtil.increment());
     		
+				employee.setEid((int)CounterLocalServiceUtil.increment());
+			
     		EmployeeLocalServiceUtil.addEmployee(employee);
     		System.out.println(employee.getAddress());
+    	}catch (SystemException e) {
+			
+			e.printStackTrace(); 
+		}
     		
     	
   
